aba2300eddcea3c50003446115364ff6bc870f2a
potocek <potocek@merica.cz>
2016-09-22 10:49:58 +0200

    overlay creator added

---
+++ src/main/java/cz/certicon/routing/algorithm/sara/preprocessing/overlay/OverlayCreator.java
@@ -0,0 +1,98 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.certicon.routing.algorithm.sara.preprocessing.overlay;
+
+import cz.certicon.routing.algorithm.sara.preprocessing.assembly.Assembler;
+import cz.certicon.routing.algorithm.sara.preprocessing.assembly.GreedyAssembler;
+import cz.certicon.routing.algorithm.sara.preprocessing.filtering.Filter;
+import cz.certicon.routing.algorithm.sara.preprocessing.filtering.NaturalCutsFilter;
+import cz.certicon.routing.data.GraphDAO;
+import cz.certicon.routing.data.SqliteGraphDAO;
+import cz.certicon.routing.model.graph.Graph;
+import cz.certicon.routing.model.graph.SaraGraph;
+import cz.certicon.routing.model.graph.preprocessing.ContractGraph;
+import cz.certicon.routing.utils.RandomUtils;
+import java.io.InputStream;
+import java.util.Properties;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ *
+ * @author Blahoslav Potoček <potocek@merica.cz>
+ */
+public class OverlayCreator {
+
+    @Getter
+    @Setter
+    public class SaraSetup {
+
+        long randomSeed = -1;
+        int maxCellSize = 500;
+        double cellRatio = 1;
+        double coreRatioInverse = 10;
+        double lowIntervalProbability = 0.03;
+        double lowerIntervalLimit = 0.6;
+    }
+
+    @Getter
+    SaraSetup setup;
+
+    public OverlayCreator() {
+        setup = new SaraSetup();
+    }
+
+    public OverlayCreator(SaraSetup setup) {
+        this.setup = setup;
+    }
+
+    public SaraGraph CreateSaraGraph() {
+        try {
+
+            if (this.setup.randomSeed > 0) {
+                RandomUtils.setSeed(this.setup.randomSeed);
+            }
+
+            Properties properties = new Properties();
+            InputStream in = getClass().getClassLoader().getResourceAsStream("spatialite.properties");
+            properties.load(in);
+            in.close();
+
+            int maxCellSize = this.setup.maxCellSize;
+            double cellRatio = this.setup.cellRatio;
+            double coreRatioInverse = this.setup.coreRatioInverse;
+            double lowIntervalProbability = this.setup.lowIntervalProbability;
+            double lowerIntervalLimit = this.setup.lowerIntervalLimit;
+            GraphDAO graphDAO = new SqliteGraphDAO(properties);
+            Graph graph = graphDAO.loadGraph();
+            Filter filter = new NaturalCutsFilter(cellRatio, coreRatioInverse, maxCellSize);
+            ContractGraph filteredGraph = filter.filter(graph);
+            Assembler assembler = new GreedyAssembler(lowIntervalProbability, lowerIntervalLimit, maxCellSize);
+            SaraGraph saraGraph = assembler.assemble(graph, filteredGraph);
+            return saraGraph;
+
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+
+    public OverlayBuilder CreateBuilder() {
+        SaraGraph sara = this.CreateSaraGraph();
+        if (sara == null) {
+            return null;
+        }
+        OverlayBuilder builder = new OverlayBuilder(sara);
+        return builder;
+    }
+
+    public OverlayBuilder CreateOverlays() {
+        OverlayBuilder builder = this.CreateBuilder();
+        if (builder != null) {
+            builder.buildOverlays();
+        }
+        return builder;
+    }
+}
