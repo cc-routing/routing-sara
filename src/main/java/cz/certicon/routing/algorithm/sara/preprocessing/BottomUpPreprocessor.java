6e3b5fb07830618edb2f3358ddd7f0b5effb10e1
Michael Bláha <blahami2@gmail.com>
2016-10-07 14:30:28 +0200

    Fix for preprocessor

--- src/main/java/cz/certicon/routing/algorithm/sara/preprocessing/BottomUpPreprocessor.java
+++ src/main/java/cz/certicon/routing/algorithm/sara/preprocessing/BottomUpPreprocessor.java
@@ -19,14 +19,19 @@ import cz.certicon.routing.model.graph.Node;
 import cz.certicon.routing.model.graph.SaraEdge;
 import cz.certicon.routing.model.graph.SaraGraph;
 import cz.certicon.routing.model.graph.SaraNode;
+import cz.certicon.routing.model.graph.preprocessing.ContractEdge;
 import cz.certicon.routing.model.graph.preprocessing.ContractGraph;
 import cz.certicon.routing.model.graph.preprocessing.ContractNode;
+import cz.certicon.routing.model.values.Time;
+import cz.certicon.routing.model.values.TimeUnits;
 import cz.certicon.routing.utils.java8.IteratorStreams;
 import cz.certicon.routing.utils.measuring.TimeLogger;
+import cz.certicon.routing.utils.measuring.TimeMeasurement;
 import cz.certicon.routing.utils.progress.EmptyProgressListener;
 import cz.certicon.routing.utils.progress.ProgressListener;
 import java.util.EnumSet;
 import java8.util.function.Predicate;
+import java8.util.function.ToIntFunction;
 
 /**
  *
@@ -55,70 +60,71 @@ public class BottomUpPreprocessor implements Preprocessor {
         TimeLogger.log( TimeLogger.Event.ASSEMBLING, TimeLogger.Command.START );
         Assembler assembler = new GreedyAssembler( input.getLowIntervalProbability(), input.getLowIntervalLimit(), input.getCellSize() );
 
-        SaraGraph bestResult = null;
-        long bestEdgeCount = Long.MAX_VALUE;
-        for ( int run = 0; run < input.getNumberOfAssemblyRuns(); run++ ) {
-
-            int currentCellSize = input.getCellSize();
-            // for i < layers
-            // - assemble graph
-            // - create sara graph
-            // - assign parents
-            SaraGraph saraGraph = null;
-            for ( int i = 0; i < input.getNumberOfLayers(); i++ ) {
-                assembler.setMaxCellSize( currentCellSize );
-                ContractGraph assembled = assembler.assemble( filteredGraph );
-                if ( saraGraph == null ) {
-                    saraGraph = new SaraGraph( EnumSet.of( Metric.LENGTH, Metric.TIME ) );
-                    for ( ContractNode node : assembled.getNodes() ) {
-                        Cell cell = new Cell( cellIdContainer.next() );
-//                    System.out.println( "putting: " + node.getId() + " -> " + cell.getId() + ", nodes=[" + StreamSupport.stream( node.getNodes() ).map( Mappers.identifiableToString ).collect( Collectors.joining( "," ) ) + "]" );
-                        for ( Node origNode : node.getNodes() ) {
-                            SaraNode saraNode = saraGraph.createNode( origNode.getId(), cell );
-                            saraNode.setCoordinate( origNode.getCoordinate() );
-                            saraNode.setTurnTable( origNode.getTurnTable() );
-                        }
-                    }
-                    for ( E edge : graph.getEdges() ) {
-                        SaraEdge saraEdge = saraGraph.createEdge( edge.getId(), edge.isOneWay(),
-                                saraGraph.getNodeById( edge.getSource().getId() ), saraGraph.getNodeById( edge.getTarget().getId() ),
-                                edge.getSourcePosition(), edge.getTargetPosition(),
-                                new Pair<>( Metric.LENGTH, edge.getLength( Metric.LENGTH ) ), new Pair<>( Metric.TIME, edge.getLength( Metric.TIME ) ) );
-                    }
-                } else {
-                    for ( ContractNode node : assembled.getNodes() ) {
-                        Cell cell = new Cell( cellIdContainer.next() );
-//                    System.out.println( "putting&getting: " + node.getId() + " -> " + cell.getId() + ", nodes=[" + StreamSupport.stream( node.getNodes() ).map( Mappers.identifiableToString ).collect( Collectors.joining( "," ) ) + "]" );
-                        for ( Node n : node.getNodes() ) {
-                            Cell parent = saraGraph.getNodeById( n.getId() ).getParent();
-                            for ( int j = 1; j < i; j++ ) {
-                                parent = parent.getParent();
+        int currentCellSize = input.getCellSize();
+        // for i < layers
+        // - assemble graph
+        // - create sara graph
+        // - assign parents
+        SaraGraph saraGraph = null;
+        for ( int i = 0; i < input.getNumberOfLayers(); i++ ) {
+            assembler.setMaxCellSize( currentCellSize );
+            ContractGraph assembled = null;
+            long bestEdgeCount = Long.MAX_VALUE;
+            for ( int run = 0; run < input.getNumberOfAssemblyRuns(); run++ ) {
+                ContractGraph assembledTmp = assembler.assemble( filteredGraph );
+                long edgeCount = IteratorStreams.stream( assembledTmp.getEdges() )
+                        .mapToInt( new ToIntFunction<ContractEdge>() {
+                            @Override
+                            public int applyAsInt( ContractEdge value ) {
+                                return value.calculateWidth();
                             }
-                            if ( !parent.hasParent() ) {
-                                parent.setParent( cell );
-                                parent.lock();
-                            }
-                        }
-                    }
-
+                        } )
+                        .sum();
+                if ( edgeCount < bestEdgeCount ) {
+                    assembled = assembledTmp;
+                    bestEdgeCount = edgeCount;
                 }
-                filteredGraph = assembled;
-                currentCellSize *= input.getCellSize();
                 progressListener.nextStep();
             }
-            long edgeCount = IteratorStreams.stream( saraGraph.getEdges() ).filter( new Predicate<SaraEdge>() {
-                @Override
-                public boolean test( SaraEdge edge ) {
-                    return !edge.getSource().getParent().equals( edge.getTarget().getParent() );
+            if ( saraGraph == null ) {
+                saraGraph = new SaraGraph( EnumSet.of( Metric.LENGTH, Metric.TIME ) );
+                for ( ContractNode node : assembled.getNodes() ) {
+                    Cell cell = new Cell( cellIdContainer.next() );
+//                    System.out.println( "putting: " + node.getId() + " -> " + cell.getId() + ", nodes=[" + StreamSupport.stream( node.getNodes() ).map( Mappers.identifiableToString ).collect( Collectors.joining( "," ) ) + "]" );
+                    for ( Node origNode : node.getNodes() ) {
+                        SaraNode saraNode = saraGraph.createNode( origNode.getId(), cell );
+                        saraNode.setCoordinate( origNode.getCoordinate() );
+                        saraNode.setTurnTable( origNode.getTurnTable() );
+                    }
                 }
-            } ).count();
-            if ( edgeCount < bestEdgeCount ) {
-                bestResult = saraGraph;
-                bestEdgeCount = edgeCount;
+                for ( E edge : graph.getEdges() ) {
+                    SaraEdge saraEdge = saraGraph.createEdge( edge.getId(), edge.isOneWay(),
+                            saraGraph.getNodeById( edge.getSource().getId() ), saraGraph.getNodeById( edge.getTarget().getId() ),
+                            edge.getSourcePosition(), edge.getTargetPosition(),
+                            new Pair<>( Metric.LENGTH, edge.getLength( Metric.LENGTH ) ), new Pair<>( Metric.TIME, edge.getLength( Metric.TIME ) ) );
+                }
+            } else {
+                for ( ContractNode node : assembled.getNodes() ) {
+                    Cell cell = new Cell( cellIdContainer.next() );
+//                    System.out.println( "putting&getting: " + node.getId() + " -> " + cell.getId() + ", nodes=[" + StreamSupport.stream( node.getNodes() ).map( Mappers.identifiableToString ).collect( Collectors.joining( "," ) ) + "]" );
+                    for ( Node n : node.getNodes() ) {
+                        Cell parent = saraGraph.getNodeById( n.getId() ).getParent();
+                        for ( int j = 1; j < i; j++ ) {
+                            parent = parent.getParent();
+                        }
+                        if ( !parent.hasParent() ) {
+                            parent.setParent( cell );
+                            parent.lock();
+                        }
+                    }
+                }
+
             }
+            filteredGraph = assembled;
+            currentCellSize *= input.getCellSize();
         }
         TimeLogger.log( TimeLogger.Event.ASSEMBLING, TimeLogger.Command.STOP );
-        return bestResult;
+        return saraGraph;
     }
 
 //    @Override
