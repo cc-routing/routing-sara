06d308b41b1a39ec568e1d253c111ce1ce56f61f
Roman Vaclavik <vaclarom@fel.cvut.cz>
2016-10-18 21:39:50 +0200

    Interface for MLD route unpacking

---
+++ src/main/java/cz/certicon/routing/algorithm/sara/query/mld/RouteUnpacker.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.certicon.routing.algorithm.sara.query.mld;
+
+import cz.certicon.routing.algorithm.sara.preprocessing.overlay.OverlayBuilder;
+import cz.certicon.routing.model.Route;
+import cz.certicon.routing.model.graph.Edge;
+import cz.certicon.routing.model.graph.Graph;
+import cz.certicon.routing.model.graph.Metric;
+import cz.certicon.routing.model.graph.Node;
+import cz.certicon.routing.model.graph.State;
+import cz.certicon.routing.utils.java8.Optional;
+import java.util.Map;
+
+/**
+ *
+ * @author Roman Vaclavik {@literal <vaclavik@merica.cz>}
+ * @param <N> node type
+ * @param <E> edge type
+ */
+public interface RouteUnpacker<N extends Node, E extends Edge> {
+
+    Optional<Route<N, E>> unpack(Graph<N, E> graph, OverlayBuilder overlayGraph, Metric metric, State<N, E> endPoint, Map<State<N, E>, State<N, E>> predecessors);
+}
