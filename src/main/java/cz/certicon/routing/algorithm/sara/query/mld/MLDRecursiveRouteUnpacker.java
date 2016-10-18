65be0eddda1312ca6895b22fcc179cde09b8439f
Roman Vaclavik <vaclarom@fel.cvut.cz>
2016-10-18 21:41:37 +0200

    Recursive MLD route unpacker

---
+++ src/main/java/cz/certicon/routing/algorithm/sara/query/mld/MLDRecursiveRouteUnpacker.java
@@ -0,0 +1,90 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cz.certicon.routing.algorithm.sara.query.mld;
+
+import cz.certicon.routing.algorithm.DijkstraAlgorithm;
+import cz.certicon.routing.algorithm.RoutingAlgorithm;
+import cz.certicon.routing.algorithm.sara.preprocessing.overlay.OverlayBuilder;
+import cz.certicon.routing.algorithm.sara.preprocessing.overlay.OverlayEdge;
+import cz.certicon.routing.algorithm.sara.preprocessing.overlay.OverlayGraph;
+import cz.certicon.routing.algorithm.sara.preprocessing.overlay.OverlayNode;
+import cz.certicon.routing.model.Route;
+import cz.certicon.routing.model.graph.Edge;
+import cz.certicon.routing.model.graph.Graph;
+import cz.certicon.routing.model.graph.Metric;
+import cz.certicon.routing.model.graph.Node;
+import cz.certicon.routing.model.graph.State;
+import cz.certicon.routing.utils.java8.Optional;
+import java.util.Map;
+
+/**
+ *
+ * @author Roman Vaclavik {@literal <vaclavik@merica.cz>}
+ * @param <N> node type
+ * @param <E> edge type
+ */
+public class MLDRecursiveRouteUnpacker<N extends Node<N, E>, E extends Edge<N, E>> implements RouteUnpacker<N, E> {
+
+    @Override
+    public Optional<Route<N, E>> unpack(Graph<N, E> graph, OverlayBuilder overlayGraph, Metric metric, State<N, E> endPoint, Map<State<N, E>, State<N, E>> predecessors) {
+        if (endPoint != null) {
+            RoutingAlgorithm dijkstra = new DijkstraAlgorithm();
+            Route.RouteBuilder<N, E> builder = Route.<N, E>builder();
+            State<N, E> currentState = endPoint;
+
+            while (currentState != null && !currentState.isFirst()) {
+                if (!(currentState.getNode() instanceof OverlayNode)) {
+                    builder.addAsFirst(currentState.getEdge());
+                } else {
+                    OverlayNode oTo = (OverlayNode) currentState.getNode();
+                    currentState = predecessors.get(currentState);
+                    if (!(currentState.getNode() instanceof OverlayNode)) {
+                        throw new IllegalStateException("OverlayNode not found. Something is wrong.");
+                    }
+                    OverlayNode oFrom = (OverlayNode) currentState.getNode();
+
+                    unpackHighLevels(dijkstra, oTo.level(), graph, overlayGraph, metric, (E) oFrom.getIncomingEdges().next(), (E) oTo.getOutgoingEdges().next(), builder);
+                }
+                currentState = predecessors.get(currentState);
+            }
+
+            Route<N, E> route = builder.build();
+            return Optional.of(route);
+        } else {
+            return Optional.empty();
+        }
+    }
+
+    private void unpackHighLevels(RoutingAlgorithm router, int level, Graph<N, E> graph, OverlayBuilder overlayGraph, Metric metric, E fromEdge, E toEdge, Route.RouteBuilder<N, E> builder) {
+        if (level == 1) {
+            unpackLowestLevel(router, graph, metric, fromEdge, toEdge, builder);
+        } else {
+            OverlayGraph oGraph = overlayGraph.getPartitions().get(level - 1).getOverlayGraph();
+            Optional<Route<N, E>> subResult = router.route(oGraph, metric, getOverlayEdgeBelow((OverlayEdge) fromEdge), getOverlayEdgeBelow((OverlayEdge) toEdge));
+            Route<N, E> subRoute = subResult.get();
+            for (int i = subRoute.getEdgeList().size() - 1; i >= 0; i -= 2) {
+                if (i == 0) {
+                    break;
+                }
+                unpackHighLevels(router, level - 1, graph, overlayGraph, metric, subRoute.getEdgeList().get(i - 2), subRoute.getEdgeList().get(i), builder);
+            }
+        }
+    }
+
+    private void unpackLowestLevel(RoutingAlgorithm router, Graph<N, E> graph, Metric metric, E fromEdge, E toEdge, Route.RouteBuilder<N, E> builder) {
+        OverlayEdge fromOverlayEdge = (OverlayEdge) fromEdge;
+        OverlayEdge toOverlayEdge = (OverlayEdge) toEdge;
+        Optional<Route<N, E>> subResult = router.route(graph, metric, fromOverlayEdge.getSaraEdge(), toOverlayEdge.getSaraEdge());
+        Route<N, E> subRoute = subResult.get();
+        for (int i = subRoute.getEdgeList().size() - 2; i >= 0; i--) {
+            builder.addAsFirst(subRoute.getEdgeList().get(i));
+        }
+    }
+
+    private OverlayEdge getOverlayEdgeBelow(OverlayEdge higherOverlayNode) {
+        return higherOverlayNode.getSource().getLowerNode().getOutgoingEdges().next();
+    }
+}
